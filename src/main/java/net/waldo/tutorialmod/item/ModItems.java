308e310108625d11d418287b0a2f1db4ece7a4d2
WaldoTheCreator <fadirizk02@gmail.com>
2025-04-28 19:19:49 -0400

    Added James Hair (No Functionality)

--- src/main/java/net/waldo/tutorialmod/item/ModItems.java
+++ src/main/java/net/waldo/tutorialmod/item/ModItems.java
@@ -1,8 +1,8 @@
 package net.waldo.tutorialmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Portal;
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -13,10 +13,15 @@ import net.waldo.tutorialmod.Tutorialmod;
 import java.util.List;
 import java.util.Optional;
 
+import static net.minecraft.item.ArmorMaterials.*;
+import static net.minecraft.item.ArmorItem.Type.*;
+
 public class ModItems {
     public static final Item PINK_GARNET = registerItem( "pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem( "raw_pink_garnet", new Item(new Item.Settings()));
     public static final Item ALI = registerItem( "ali", new Item(new Item.Settings().food(new FoodComponent(1,1,true,1, Optional.empty(), List.of()))));
+    public static final Item TIME_WASTER = registerItem("time_waster", new Item(new Item.Settings()));
+    public static final Item JAMES_WIG = registerItem("james_wig", new ArmorItem(DIAMOND,HELMET,new Item.Settings()));
 
 
 
@@ -30,9 +35,13 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(TIME_WASTER);
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
             entries.add(ALI);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(JAMES_WIG);
         });
     }
 }
