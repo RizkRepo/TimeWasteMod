7a1076e0e0cd8ccfc73d82f9792067700f2ea2d8
WaldoTheCreator <fadirizk02@gmail.com>
2025-04-28 21:06:30 -0400

    ModBlock Class (WIP)

--- src/main/java/net/waldo/tutorialmod/Tutorialmod.java
+++ src/main/java/net/waldo/tutorialmod/Tutorialmod.java
@@ -2,6 +2,7 @@ package net.waldo.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.waldo.tutorialmod.block.ModBlocks;
 import net.waldo.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,5 +23,6 @@ public class Tutorialmod implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
