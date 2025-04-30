b4c3d9c0d8aaf1c95f0a6f79861242d1da4879f6
WaldoTheCreator <fadirizk02@gmail.com>
2025-04-30 17:08:31 -0400

    Added Tags and Loot Tables for Blocks
    -Tags Determine What tool is required to break a block as well as what level of tool (iron, diamond, etc.)
    -Loot Table: Determines What drops from a specific source

--- src/main/java/net/waldo/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/waldo/tutorialmod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.waldo.tutorialmod.block;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.block.Block;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.waldo.tutorialmod.Tutorialmod;
 
 public class ModBlocks {
@@ -19,6 +21,12 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.STONE)));
     public static final Block ALI_CUBE = registerBlock("ali_cube",
             new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.BONE)));
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).hardness(2f).requiresTool().sounds(BlockSoundGroup.DRIPSTONE_BLOCK)));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
@@ -37,6 +45,8 @@ public class ModBlocks {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
             entries.add(ModBlocks.ALI_CUBE);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
