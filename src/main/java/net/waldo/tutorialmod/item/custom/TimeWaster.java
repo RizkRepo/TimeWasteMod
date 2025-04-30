c7ba64faf207bc3d3c4d72fe9d075bd6d25179d1
WaldoTheCreator <fadirizk02@gmail.com>
2025-04-30 18:12:50 -0400

    TimeWaster Class
    -TimeWaster will allow for functionality of the TimeWaster Item

---
+++ src/main/java/net/waldo/tutorialmod/item/custom/TimeWaster.java
@@ -0,0 +1,9 @@
+package net.waldo.tutorialmod.item.custom;
+
+import net.minecraft.item.Item;
+
+public class TimeWaster extends Item {
+    public TimeWaster(Settings settings){
+        super(settings);
+    }
+}
